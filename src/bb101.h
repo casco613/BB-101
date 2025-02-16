b5bba7603d35f41ce5a2f2ddd1a4ae0542d8cada
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2025-02-15 23:25:29 -0600

    feat: Agregado stdbool para la utilizacion de true con valor de 1 para uso en bucles infinitos

--- src/bb101.h
+++ src/bb101.h
@@ -1,15 +1,17 @@
 #ifndef BB101_H
 #define BB101_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef char* string;
+typedef char *string;
 
 string obtener_texto(const string msg);
-int obtener_entero(const string msg); 
+int obtener_entero(const string msg);
+char obtener_caracter(const string msg);
 float obtener_decimal(const string msg);
 double obtener_decimal_grande(const string msg);
 
-#endif 
+#endif
